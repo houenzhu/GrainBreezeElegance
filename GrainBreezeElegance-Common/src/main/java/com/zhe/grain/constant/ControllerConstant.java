38e5f32043de28dea5037d590af84e2d5022e0c5
houenzhu <houenzhu@qq.com>
2025-01-11 15:50:55 +0800

    新建控制器的静态常量

---
+++ GrainBreezeElegance-Common/src/main/java/com/zhe/grain/constant/ControllerConstant.java
@@ -0,0 +1,11 @@
+package com.zhe.grain.constant;
+
+/**
+ * @version 1.0
+ * @Author 朱厚恩
+ */
+
+public class ControllerConstant {
+    // 定义接口前缀
+    public static final String API_PREFIX = "/grain/";
+}
