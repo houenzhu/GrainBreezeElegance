c15c6f6dbf21ef651dd0de43daa7a588a4f065cf
houenzhu <houenzhu@qq.com>
2025-02-21 14:53:26 +0800

    使用自定义校验器

--- GrainBreezeElegance-Main-10011/src/main/java/com/zhe/grain/vo/user/RegisterUserVO.java
+++ GrainBreezeElegance-Main-10011/src/main/java/com/zhe/grain/vo/user/RegisterUserVO.java
@@ -1,10 +1,14 @@
 package com.zhe.grain.vo.user;
 
+import com.zhe.grain.exception.vaild.SaveGroup;
+import com.zhe.grain.exception.vaild.UpdateGroup;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -22,9 +26,18 @@ public class RegisterUserVO implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @NotBlank(message = "请输入用户名", groups = {SaveGroup.class, UpdateGroup.class})
     private String username;
+
+    @NotBlank(message = "请选择性别", groups = {SaveGroup.class, UpdateGroup.class})
     private String sex;
+
+    @NotBlank(message = "请输入手机号", groups = {SaveGroup.class, UpdateGroup.class})
     private String phone;
+
+    @NotBlank(message = "请输入邮箱", groups = {SaveGroup.class, UpdateGroup.class})
     private String email;
+
+    @NotBlank(message = "请选择用户类型", groups = {SaveGroup.class, UpdateGroup.class})
     private String userType;
 }
