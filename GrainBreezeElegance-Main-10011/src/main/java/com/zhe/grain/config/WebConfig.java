28a4d5ed5a4834e217569a5bcf3de8c5329de24e
houenzhu <houenzhu@qq.com>
2025-02-21 14:57:10 +0800

    使用Lombok构造器注入

--- GrainBreezeElegance-Main-10011/src/main/java/com/zhe/grain/config/WebConfig.java
+++ GrainBreezeElegance-Main-10011/src/main/java/com/zhe/grain/config/WebConfig.java
@@ -1,10 +1,9 @@
 package com.zhe.grain.config;
 
-import com.zhe.grain.interceptor.AdminAccessInterceptor;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,10 +15,11 @@ import java.util.List;
  */
 
 @Configuration
+@AllArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private AdminArgumentResolver adminArgumentResolver;
+    private final AdminArgumentResolver adminArgumentResolver;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(adminArgumentResolver);
