5306c4c7c5b37471f33aad1019f9f67f6669d097
houenzhu <houenzhu@qq.com>
2025-02-21 14:52:21 +0800

    使用Record自定义接受前端数据

---
+++ GrainBreezeElegance-Main-10011/src/main/java/com/zhe/grain/record/AgricultureConsultationsRecord.java
@@ -0,0 +1,10 @@
+package com.zhe.grain.record;
+
+/**
+ * @version 1.0
+ * @Author 朱厚恩
+ */
+
+public record AgricultureConsultationsRecord(String title, Integer selectTab, String content, String cover) {
+
+}
