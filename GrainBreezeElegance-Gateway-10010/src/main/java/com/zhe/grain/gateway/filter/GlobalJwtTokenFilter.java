c22fee8e0b351940ca48ad171d13b941866f3096
houenzhu <houenzhu@qq.com>
2025-02-28 15:43:54 +0800

    简化遍历白名单代码

--- GrainBreezeElegance-Gateway-10010/src/main/java/com/zhe/grain/gateway/filter/GlobalJwtTokenFilter.java
+++ GrainBreezeElegance-Gateway-10010/src/main/java/com/zhe/grain/gateway/filter/GlobalJwtTokenFilter.java
@@ -1,15 +1,12 @@
 package com.zhe.grain.gateway.filter;
 
 import com.zhe.grain.gateway.utils.MonoResponseUtil;
-import com.zhe.grain.gateway.utils.RedisCache;
 import com.zhe.grain.utils.JwtUtil;
 import com.zhe.grain.utils.Result;
 import com.zhe.grain.utils.ResultMsgEnum;
-import com.zhe.grain.utils.design.iterator.AnyClassBox;
 import io.jsonwebtoken.Claims;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -19,7 +16,7 @@ import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
-import java.util.Iterator;
+import java.util.List;
 
 /**
  * @version 1.0
@@ -29,10 +26,6 @@ import java.util.Iterator;
 @Component
 @Slf4j
 public class GlobalJwtTokenFilter implements GlobalFilter, Ordered {
-
-    @Autowired
-    private RedisCache redisCache;
-
 
     // 定义白名单路径
     private static final String[] UN_CHECK_PATHS = {
@@ -46,13 +39,9 @@ public class GlobalJwtTokenFilter implements GlobalFilter, Ordered {
         String path = request.getURI().getPath();
         log.info("请求: {}, 经过网关过滤器", path);
         // 使用iterator模式
-        AnyClassBox<String> stringAnyClassBox = new AnyClassBox<>(5);
-        stringAnyClassBox.appendAll(Arrays.asList(UN_CHECK_PATHS));
-        Iterator<String> iterator = stringAnyClassBox.iterator();
-        while (iterator.hasNext()) {
-            if (path.equals(iterator.next())) {
-                return chain.filter(exchange);
-            }
+        List<String> whitePath = Arrays.asList(UN_CHECK_PATHS);
+        if (whitePath.contains(path)) {
+            return chain.filter(exchange);
         }
         String token = request.getHeaders().getFirst("Authorization");
         if (StringUtils.isBlank(token)) {
@@ -60,8 +49,8 @@ public class GlobalJwtTokenFilter implements GlobalFilter, Ordered {
             return MonoResponseUtil.monoResponse(exchange.getResponse(),
                     Result.error(ResultMsgEnum.UNAUTHORIZED, null));
         }
-        token = token.split(" ")[1];
-        if (!checkToken(token)) {
+        String tokenSuffix = token.split(" ")[1];
+        if (!checkToken(tokenSuffix)) {
             return MonoResponseUtil.monoResponse(exchange.getResponse(),
                     Result.error(ResultMsgEnum.UNAUTHORIZED, "您的登录信息有误!请刷新页面", null));
         }
